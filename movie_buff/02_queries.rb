876a7bcea043b084d68d73dd7f300dc3babdaf57
your name here <johnobrien8642@gmail.com>
2020-10-13 14:54:02 -0400

    02_queries #bad_years written and passed testing

--- movie_buff/02_queries.rb
+++ movie_buff/02_queries.rb
@@ -10,7 +10,8 @@ end
 
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
-
+  Movie
+   .group(:yr).having('MAX(score) < 8').pluck(:yr)
 end
 
 def cast_list(title)
